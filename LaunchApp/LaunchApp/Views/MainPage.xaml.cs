aeba3bd436b4abf2f859528cccebae0fdc69dca6
jerrynixon <jerry@nixoncorp.com>
2016-11-04 11:09:46 -0600

    Layout

--- LaunchApp/LaunchApp/Views/MainPage.xaml.cs
+++ LaunchApp/LaunchApp/Views/MainPage.xaml.cs
@@ -8,7 +8,23 @@ namespace LaunchApp.Views
         public MainPage()
         {
             InitializeComponent();
+
             NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+
+        private async void BackgroundList_ItemClick(Object sender, ItemClickEventArgs e)
+        {
+            await MyContentDialog.ShowAsync();
+        }
+
+        private async void MyContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            // TODO
+        }
+
+        private void MyContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            // TODO
         }
     }
 }
