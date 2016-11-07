9f57b95d7684a44fcd81d9b257acae82ba266a1d
Adam Scott <adam@launchjourney.com>
2016-11-07 18:09:27 -0500

    adding image to push

--- SendImageTo/index.js
+++ SendImageTo/index.js
@@ -3,7 +3,8 @@ var fs = require("fs");
 var Twitter = require('twitter');
 
 module.exports = function (context, myBlob) {
-    var container = "workitems";
+    var containerName = "workitems";
+    var destinationFileNameTarget = "file.jpg";
 
     var client = new Twitter({
         consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -14,45 +15,53 @@ module.exports = function (context, myBlob) {
 
     context.log("Node.js blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
     context.log('Node.js blob trigger function processed blob', myBlob);
+    //context.log('Node.js blob type of',  typeOf(myBlob));
 
     var blobName = context.bindingData.name;
     var blobSvc = azure.createBlobService();
 
     // Load your image
     var data = require('fs').readFileSync('image.jpg');
+    var stream = fs.createReadStream(fileNameTarget).pipe(blobService.createWriteStreamToAppendBlob(containerName, blobName));
 
-    // Make post request on media endpoint. Pass file data as media parameter
-    client.post('media/upload', {media: myBlob}, function(error, media, response) {
+    var writable = fs.createWriteStream(destinationFileNameTarget);
+    blobService.createReadStream(containerName, blobName).pipe(writable);
 
-        if (!error) {
-            // If successful, a media object will be returned.
-            context.log('media uploaded', media);
+    writeable.on('finish', function(){  
+        var twitterImage = require('fs').readFileSync(destinationFileNameTarget);
 
-            // Lets tweet it
-            var status = {
-              status: 'I am a tweet',
-              media_ids: media.media_id_string // Pass the media id string
-            }
+        // Make post request on media endpoint. Pass file data as media parameter
+        client.post('media/upload', {media: twitterImage}, function(error, media, response) {
+            if (!error) {
+                // If successful, a media object will be returned.
+                context.log('media uploaded', media);
 
-            client.post('statuses/update', status, function(error, tweet, response) {
-                if (!error) {
-                    console.log('tweet sent', tweet);
-                    console.log('tweet response', response);
-                    blobSvc.deleteBlob(container, blobName, function(error, response){
-                        if(!error){
-                            // Blob has been deleted
-                            context.log('deleted');
-                            context.done();
-                        } else {
-                            context.log('error deleting');
-                            context.done();
-                        }
-                    });
+                // Lets tweet it
+                var status = {
+                status: 'I am a tweet',
+                media_ids: media.media_id_string // Pass the media id string
                 }
-            });
-        } else {
-            context.log('error uploading', error);
-            context.done();
-        }
+
+                client.post('statuses/update', status, function(error, tweet, response) {
+                    if (!error) {
+                        console.log('tweet sent', tweet);
+                        console.log('tweet response', response);
+                        blobSvc.deleteBlob(containerName, blobName, function(error, response){
+                            if(!error){
+                                // Blob has been deleted
+                                context.log('deleted');
+                                context.done();
+                            } else {
+                                context.log('error deleting');
+                                context.done();
+                            }
+                        });
+                    }
+                });
+            } else {
+                context.log('error uploading', error);
+                context.done();
+            }
+        });
     });
 };
