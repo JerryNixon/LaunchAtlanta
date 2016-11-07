a08af1e500d78464a06128d2746452c6e9e95486
Adam Scott <adam@launchjourney.com>
2016-11-07 14:41:21 -0500

    adding container and blob name

--- SendImageTo/index.js
+++ SendImageTo/index.js
@@ -1,11 +1,13 @@
 ﻿var azure = require('azure-storage');
 
 module.exports = function (context, myBlob) {
+    var container = "workitems";
     context.log("Node.js blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
+    var blobName = context.bindingData.name;
     context.log(JSON.stringify(context));
     context.log(JSON.stringify(context));
     var blobSvc = azure.createBlobService();
-    blobSvc.deleteBlob(containerName, 'myblob', function(error, response){
+    blobSvc.deleteBlob(containerName, blobName, function(error, response){
         if(!error){
             // Blob has been deleted
             context.log('deleted')
