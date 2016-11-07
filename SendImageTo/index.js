3d403d3385a7f762bcd78f19e670a6f87f3113b7
Adam Scott <adam@launchjourney.com>
2016-11-07 14:06:51 -0500

    updating code

--- SendImageTo/index.js
+++ SendImageTo/index.js
@@ -1,4 +1,13 @@
 ﻿module.exports = function (context, myBlob) {
     context.log("Node.js blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
+    context.log(JSON.stringify(context));
+    context.log(JSON.stringify(context));
+    var blobSvc = azure.createBlobService();
+    blobSvc.deleteBlob(containerName, 'myblob', function(error, response){
+        if(!error){
+            // Blob has been deleted
+            context.log('deleted')
+        }
+    });
     context.done();
 };
