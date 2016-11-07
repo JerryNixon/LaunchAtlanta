186e0e8ff94e3a550f1a6b559ce340bf9d8eb7c8
Adam Scott <adam@launchjourney.com>
2016-11-07 14:14:35 -0500

    updating require statement

--- SendImageTo/index.js
+++ SendImageTo/index.js
@@ -1,4 +1,6 @@
-﻿module.exports = function (context, myBlob) {
+﻿var azure = require('azure-storage');
+
+module.exports = function (context, myBlob) {
     context.log("Node.js blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
     context.log(JSON.stringify(context));
     context.log(JSON.stringify(context));
